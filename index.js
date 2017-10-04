8bb85e4bc4f8c95628c2e97e951db7cb02c5fec2
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2017-10-04 16:49:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var katzDeliLine = []
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(name)
+  var position = katzDeliLine[name] + 1
+  console.log{`Welcome, ${name}. You are number ${position} in line.`}
+}
+function nowServing(katzDeliLine) {
+  var serving = katzDeliLine[0]
+  console.log(`Currently serving ${serving}`.)
+  katzDeliLine.shift()
+}
