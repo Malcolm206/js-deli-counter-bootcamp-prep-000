6f6539c61f7b21cbdc570ae71ff7cce12c79a549
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2017-10-04 17:12:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeliLine = []
 function takeANumber (katzDeliLine, name) {
   katzDeliLine.push(name)
-  var position = katzDeliLine.indexof(name) + 1
+  var position = katzDeliLine.indexOf(name) + 1
   console.log(`Welcome, ${name}. You are number ${position} in line.`)
 }
 function nowServing(katzDeliLine) {
