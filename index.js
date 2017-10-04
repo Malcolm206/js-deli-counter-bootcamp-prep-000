38a88567f7ad5461b5515b2ef7124ab53df02642
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2017-10-04 17:33:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(katzDeliLine) {
   else {
     var line = `The line is currently: `
     for(var i = 1; i < katzDeliLine.length; i++){
-      line = line + `${i}. ${katzDeliLine[i]}`
+      line = line + `${i}. ${katzDeliLine[i - 1]}`
     }
     return line
   }
